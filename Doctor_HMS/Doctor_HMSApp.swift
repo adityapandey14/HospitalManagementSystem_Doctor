0e90b119cf80483d19f26405ef9bc54d2b7e2e35
Aditya Pandey <anshu61796@gmail.com>
2024-04-19 14:25:36 +0530

    Initial Commit

---
+++ Doctor_HMS/Doctor_HMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doctor_HMSApp.swift
+//  Doctor_HMS
+//
+//  Created by Aditya Pandey on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Doctor_HMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
