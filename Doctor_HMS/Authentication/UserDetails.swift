cd7c7cd0f3f7e4cf5d8e276e6433b9788ddaae74
Aditya Pandey <anshu61796@gmail.com>
2024-04-22 00:48:15 +0530

    Added login and sign up page

---
+++ Doctor_HMS/Authentication/UserDetails.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetails.swift
+//  Doctor_HMS
+//
+//  Created by Aditya Pandey on 21/04/24.
+//
+
+import SwiftUI
+
+struct UserDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetails()
+}
